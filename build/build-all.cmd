6a58bde2d5341122c3787e7c085b24295fd197e9
Daniel Keep <daniel.keep@gmail.com>
2011-02-06 23:56:40 +1100

    The Great Rename, Part 2.
    Renamed files.

--- build/build-all.cmd
+++ build/build-all.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
 CALL dmdenv 1.057-tango-0.99.9
-xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/LexFile.deps +O.xf/LexFile.objs +o..\bin\LexFile -I../src -J../src/import ../src/meme/lexer/LexFile.d
-xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/ParseFile.deps +O.xf/ParseFile.objs +o..\bin\ParseFile -I../src -J../src/import ../src/meme/parser/ParseFile.d
+xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/LexFile.deps +O.xf/LexFile.objs +o..\bin\LexFile -I../src -J../src/import ../src/ouro/lexer/LexFile.d
+xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/ParseFile.deps +O.xf/ParseFile.objs +o..\bin\ParseFile -I../src -J../src/import ../src/ouro/parser/ParseFile.d
