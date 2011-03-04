881aae91f32cbe11864145a3253061114db3296a
Daniel Keep <daniel.keep@gmail.com>
2011-03-04 19:09:07 +1100

    Fixed build-all.cmd to stop on failure.

--- build/build-all.cmd
+++ build/build-all.cmd
@@ -1,5 +1,14 @@
 @ECHO OFF
 CALL dmdenv 1.057-tango-0.99.9
 xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/LexFile.deps +O.xf/LexFile.objs +o..\bin\LexFile -I../src -J../src/import ../src/ouro/lexer/LexFile.d
+if %errorlevel% neq 0 goto fail
 xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/ParseFile.deps +O.xf/ParseFile.objs +o..\bin\ParseFile -I../src -J../src/import ../src/ouro/parser/ParseFile.d
+if %errorlevel% neq 0 goto fail
 xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/SemFile.deps +O.xf/SemFile.objs +o..\bin\SemFile -I../src -J../src/import ../src/ouro/sem/SemFile.d
+if %errorlevel% neq 0 goto fail
+goto eof
+
+:fail
+exit /b 1
+
+:eof
