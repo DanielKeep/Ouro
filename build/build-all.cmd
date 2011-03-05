4dbdd9a1ef18997a73dab53ea685b45cacb9ab09
Daniel Keep <daniel.keep@gmail.com>
2011-03-05 22:22:22 +1100

    Refactored build-all.cmd.

--- build/build-all.cmd
+++ build/build-all.cmd
@@ -1,11 +1,16 @@
 @ECHO OFF
 CALL dmdenv 1.057-tango-0.99.9
-xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/LexFile.deps +O.xf/LexFile.objs +o..\bin\LexFile -I../src -J../src/import %1 %2 %3 %4 %5 %6 %7 %8 %9 ../src/ouro/lexer/LexFile.d
+set ARGS=-g -debug -unittest "-version=Unittest" +xtango -I../src -J../src/import
+
+xfbuild %ARGS% +D.xf/LexFile.deps +O.xf/LexFile.objs +o..\bin\LexFile %1 %2 %3 %4 %5 %6 %7 %8 %9 ../src/ouro/lexer/LexFile.d
 if %errorlevel% neq 0 goto fail
-xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/ParseFile.deps +O.xf/ParseFile.objs +o..\bin\ParseFile -I../src -J../src/import %1 %2 %3 %4 %5 %6 %7 %8 %9 ../src/ouro/parser/ParseFile.d
+
+xfbuild %ARGS% +D.xf/ParseFile.deps +O.xf/ParseFile.objs +o..\bin\ParseFile %1 %2 %3 %4 %5 %6 %7 %8 %9 ../src/ouro/parser/ParseFile.d
 if %errorlevel% neq 0 goto fail
-xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/SemFile.deps +O.xf/SemFile.objs +o..\bin\SemFile -I../src -J../src/import %1 %2 %3 %4 %5 %6 %7 %8 %9 ../src/ouro/sem/SemFile.d
+
+xfbuild %ARGS% +D.xf/SemFile.deps +O.xf/SemFile.objs +o..\bin\SemFile %1 %2 %3 %4 %5 %6 %7 %8 %9 ../src/ouro/sem/SemFile.d
 if %errorlevel% neq 0 goto fail
+
 goto eof
 
 :fail
