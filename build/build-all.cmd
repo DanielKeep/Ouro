adc00936399a0908828a06f87a72da407950d7df
Daniel Keep <daniel.keep@gmail.com>
2011-03-18 20:47:42 +1100

    Added progress messages to build-all.cmd.

--- build/build-all.cmd
+++ build/build-all.cmd
@@ -2,12 +2,15 @@
 CALL dmdenv 1.057-tango-0.99.9
 set ARGS=-g -debug -unittest "-version=Unittest" +xtango -I../src -J../src/import
 
+echo Compiling LexFile
 xfbuild %ARGS% +D.xf/LexFile.deps +O.xf/LexFile.objs +o..\bin\LexFile %1 %2 %3 %4 %5 %6 %7 %8 %9 ../src/ouro/lexer/LexFile.d
 if %errorlevel% neq 0 goto fail
 
+echo Compiling ParseFile
 xfbuild %ARGS% +D.xf/ParseFile.deps +O.xf/ParseFile.objs +o..\bin\ParseFile %1 %2 %3 %4 %5 %6 %7 %8 %9 ../src/ouro/parser/ParseFile.d
 if %errorlevel% neq 0 goto fail
 
+echo Compiling SemFile
 xfbuild %ARGS% +D.xf/SemFile.deps +O.xf/SemFile.objs +o..\bin\SemFile %1 %2 %3 %4 %5 %6 %7 %8 %9 ../src/ouro/sem/SemFile.d
 if errorlevel 1 goto fail
 
